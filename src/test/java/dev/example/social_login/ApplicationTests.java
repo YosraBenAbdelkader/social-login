52cf5af118c4bd8bfeee875da20c4ff0bf1d36b8
Yosra <yosra_benabdelkader@carrefour.com>
2024-11-07 00:51:42 +0100

    Initial commit

---
+++ src/test/java/dev/example/social_login/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.example.social_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
